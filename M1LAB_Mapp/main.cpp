a16d2ff78aebee9e143dca81f52dee73ef688dd2
Mapp-Maxey <mappm7303@student.faytechcc.edu>
2023-08-28 11:25:51 -0400

    Started M1LAB

---
+++ M1LAB_Mapp/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+CSC 134
+M1LAB - Apple Orchard
+Maxey Mapp
+8/28/23
+Simple interactive store.
+*/
+
+int main()
+{
+    string name = "Mapp ";
+    cout << "Welcome to the " << name << "Orchard." << endl;
+    return 0;
+}
